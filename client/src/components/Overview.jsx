2113f3dbe6f16e38c5344eac4c0cf08322a833d5
Preston Carroll <prestondcarroll@gmail.com>
2021-07-23 16:01:27 -0700

    added getting average review score

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,15 +1,67 @@
+/* eslint-disable padded-blocks */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable spaced-comment */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/button-has-type */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import $ from 'jquery';
+
+// Star Rating
+// Input:
+
+
+const calculateAverage = (data) => {
+  //for every key in ratings,
+  //have running total of every review * 5 stars
+  //have running total of every review * respective star rating
+
+  //divide numberstars/ total stars
+  //return average
+
+  const { ratings } = data;
+  let totalStars = 0;
+  let numberStars = 0;
+  Object.keys(ratings).forEach((key) => {
+    const keyVal = parseInt(key, 10);
+    totalStars += ratings[key] * 5;
+    numberStars += keyVal * ratings[key];
+  });
+
+  const average = (numberStars / totalStars) * 5;
+  const rounded = (Math.round(average * 4) / 4).toFixed(2);
+  return rounded;
+
+};
 
 function Overview() {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  const [reviewMeta, setReviewMeta] = useState('reviewMeta');
+  const [reviewAverage, setAverage] = useState('reviewAverage');
+
+  useEffect(() => {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:3000/reviews',
+      success: (data) => {
+        console.log('reviews are: ');
+        console.log(data);
+        setReviewMeta(data);
+        setAverage(calculateAverage(data));
+
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }, []);
+
+
 
   return (
     <div>
       <p>Product Overview will be here</p>
+      <p>Average is: {reviewAverage}</p>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-var express = require('express');
+const express = require('express');
 const jsdom = require('jsdom');
-const dom = new jsdom.JSDOM("")
-var $ = require('jquery')(dom.window);
+
+const dom = new jsdom.JSDOM('');
+const $ = require('jquery')(dom.window);
+
 const PORT = 3000;
 const API_KEY = require('../config.js');
 
-
-$.ajaxPrefilter(function (settings, _, jqXHR) {
+$.ajaxPrefilter((settings, _, jqXHR) => {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 console.log(API_KEY);
 
-
-var app = express();
+const app = express();
 
 // app.use( express.json() );
 
 // UNCOMMENT FOR REACT
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(`${__dirname}/../client/dist`));
 
 app.get('/products', (req, res) => {
   $.ajax({
@@ -30,8 +30,23 @@ app.get('/products', (req, res) => {
     error: (err) => {
       console.log(err);
       res.sendStatus(500, err);
-    }
-  })
+    },
+  });
+});
+
+app.get('/reviews', (req, res) => {
+  $.ajax({
+    method: 'GET',
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/reviews/meta?product_id=20100',
+    success: (data) => {
+      console.log(data);
+      res.send(data);
+    },
+    error: (err) => {
+      console.log(err);
+      res.sendStatus(500, err);
+    },
+  });
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
