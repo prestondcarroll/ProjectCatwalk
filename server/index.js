eca8634f6724581fc227d4a1836f4e4ff6cc8187
Jessica Hu <ruiruihu001@gmail.com>
2021-07-24 15:04:29 -0700

    related Products Get

--- server/index.js
+++ server/index.js
@@ -19,10 +19,12 @@ const app = express();
 // UNCOMMENT FOR REACT
 app.use(express.static(`${__dirname}/../client/dist`));
 
+const baseUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea';
+
 app.get('/products', (req, res) => {
   $.ajax({
     method: 'GET',
-    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products/',
+    url: baseUrl + '/products/',
     success: (data) => {
       res.send(data);
     },
@@ -32,10 +34,36 @@ app.get('/products', (req, res) => {
   });
 });
 
+app.get('/products/:product_id/related', (req, res) => {
+  const productId = req.params.product_id;
+  $.ajax({
+    method: 'GET',
+    url: baseUrl + `/products/${productId}/related/`,
+    success: (data) => {
+      let promises = data.map(id => {
+        return new Promise((resolve, reject) => {
+          $.ajax({
+            method: 'GET',
+            url: baseUrl + `/products/${id}/`,
+            success: (product) => {
+              resolve(product);
+            },
+            error: (err) => reject(err)
+          });
+        });
+      });
+      Promise.all(promises).then(values => res.send(values));
+    },
+    error: (err) => {
+      res.sendStatus(500, err);
+    }
+  });
+});
+
 app.get('/reviews', (req, res) => {
   $.ajax({
     method: 'GET',
-    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/reviews/meta?product_id=20100',
+    url: baseUrl + '/reviews/meta?product_id=20100',
     success: (data) => {
       console.log(data);
       res.send(data);
@@ -46,5 +74,6 @@ app.get('/reviews', (req, res) => {
     },
   });
 });
+
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
