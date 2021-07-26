33b1d34a9131c5c8099a91a7159b19c9a8c869f3
Jessica Hu <ruiruihu001@gmail.com>
2021-07-26 16:49:40 -0700

    ratings finished

--- client/src/components/RelatedProductItem.jsx
+++ client/src/components/RelatedProductItem.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import $ from 'jquery';
 import styled from 'styled-components';
+import StarRating from './StarRating.jsx';
+import calculateAverage from '../utils/calculateAverage.js';
 
 const StyledImage = styled.img`
   width: 260px;
@@ -9,12 +11,14 @@ const StyledImage = styled.img`
 
 const RelatedProductItem = (props) => {
   // console.log(props.product);
+  let rating = calculateAverage(props.product.reviews);
   return (
     <div>
       <StyledImage src={props.product.results[0].photos[0].thumbnail_url || 'https://via.placeholder.com/260x300'} alt={props.product.name} />
       <h5>{props.product.category}</h5>
       <div>{props.product.name}</div>
       <p>${props.product.default_price}</p>
+      <StarRating rating={rating}/>
     </div>
   );
 };
