c0439b55504084f3ba7ac66fd83ddbb41a9153a3
Jessica Hu <ruiruihu001@gmail.com>
2021-07-26 21:41:11 -0700

    border

--- client/src/components/RelatedProductItem.jsx
+++ client/src/components/RelatedProductItem.jsx
@@ -8,18 +8,22 @@ const StyledImage = styled.img`
   width: 260px;
   height: 300px;
 `
+const Card = styled.div`
+  border-style: groove;
+  margin-right: 50px;
+`
 
 const RelatedProductItem = (props) => {
   // console.log(props.product);
   let rating = calculateAverage(props.product.reviews);
   return (
-    <div>
+    <Card>
       <StyledImage src={props.product.results[0].photos[0].thumbnail_url || 'https://via.placeholder.com/260x300'} alt={props.product.name} />
       <h5>{props.product.category}</h5>
       <div>{props.product.name}</div>
       <p>${props.product.default_price}</p>
       <StarRating rating={rating}/>
-    </div>
+    </Card>
   );
 };
 
