bbfce33fc4067d0b0662e71a11a39f8461393b79
Jessica Hu <ruiruihu001@gmail.com>
2021-07-27 00:23:05 -0700

    carousel done

--- client/src/components/RelatedProductItem.jsx
+++ client/src/components/RelatedProductItem.jsx
@@ -5,20 +5,20 @@ import StarRating from './StarRating.jsx';
 import calculateAverage from '../utils/calculateAverage.js';
 
 const StyledImage = styled.img`
-  width: 260px;
-  height: 300px;
+  width: 200px;
+  height: 200px;
 `
 const Card = styled.div`
   border-style: groove;
-  margin-right: 50px;
+  margin-left: 10px;
+  margin-right: 10px;
 `
 
 const RelatedProductItem = (props) => {
-  // console.log(props.product);
   let rating = calculateAverage(props.product.reviews);
   return (
     <Card>
-      <StyledImage src={props.product.results[0].photos[0].thumbnail_url || 'https://via.placeholder.com/260x300'} alt={props.product.name} />
+      <StyledImage src={props.product.results[0].photos[0].thumbnail_url || 'https://via.placeholder.com/300x300'} alt={props.product.name} />
       <h5>{props.product.category}</h5>
       <div>{props.product.name}</div>
       <p>${props.product.default_price}</p>
