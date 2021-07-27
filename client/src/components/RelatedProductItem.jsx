0b765e07c7547befb7bf65f6002b605f33a50ec2
Jessica Hu <ruiruihu001@gmail.com>
2021-07-27 12:08:04 -0700

    open modal window

--- client/src/components/RelatedProductItem.jsx
+++ client/src/components/RelatedProductItem.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import StarRating from './StarRating.jsx';
 import calculateAverage from '../utils/calculateAverage.js';
 import { FaStar } from 'react-icons/fa';
+import Modal from 'react-modal';
 
 const StyledImage = styled.img`
   width: 200px;
@@ -16,11 +17,27 @@ const Card = styled.div`
   position: relative;
 `
 
+
+Modal.setAppElement('#root');
 const RelatedProductItem = (props) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   let rating = calculateAverage(props.product.reviews);
+
   return (
     <Card>
-      <FaStar size={20} style={{ fill: '#e4e5e9', position: 'absolute', top: 0, right: 0 , stroke: "#ffc107", strokeWidth: 20 }} />
+      <FaStar size={20} style={{ fill: '#e4e5e9', position: 'absolute', top: 0, right: 0 , stroke: "#ffc107", strokeWidth: 20 }} onClick={() => setModalIsOpen(true)} />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style = {
+          {
+            overlay: { backgroundColor: '#e4e5e9' },
+            content: { color: '#ffc107'}
+          }
+        }
+        >
+        <h2>Modal title</h2>
+      </Modal>
       <StyledImage src={props.product.results[0].photos[0].thumbnail_url || 'https://via.placeholder.com/300x300'} alt={props.product.name} />
       <h5>{props.product.category}</h5>
       <div>{props.product.name}</div>
