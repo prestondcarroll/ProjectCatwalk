aa119f26b9468f5af016e7c492d152de0cb70226
Jessica Hu <ruiruihu001@gmail.com>
2021-07-28 00:47:29 -0700

    outfit render

---
+++ client/src/components/Outfit.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import OutfitItem from './OutfitItem.jsx';
+import styled from 'styled-components';
+import $ from 'jquery';
+
+const CarouselContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-family: Helvetica, Arial, sans-serif;
+  margin-right: 25%;
+  margin-left: 25%;
+  align-items: center;
+  justify-content: center;
+`
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const Outfit = (props) => {
+  const [productIndex, setProductIndex] = useState(0);
+  const [isLeftButtonShown, setIsLeftButtonShown] = useState(false);
+  const [isRightButtonShown, setIsRightButtonShown] = useState(true);
+
+  const CAROUSEL_WIDTH = 3;
+
+  const handleClick = (isRight) => {
+    if (isRight) {
+      setProductIndex(productIndex + 1)
+      setIsLeftButtonShown(true);
+      if (productIndex + 1 + CAROUSEL_WIDTH >= props.outfits.length) {
+        setIsRightButtonShown(false);
+      } else {
+        setIsRightButtonShown(true);
+      }
+    } else {
+      setProductIndex(productIndex - 1)
+      setIsRightButtonShown(true);
+      if (productIndex - 1 <= 0) {
+        setIsLeftButtonShown(false);
+      } else {
+        setIsLeftButtonShown(true);
+      }
+    }
+  }
+
+  return (
+    <div>
+      <h5>YOUR OUTFIT</h5>
+      <CarouselContainer>
+        <button onClick={() => handleClick(false)} style={{visibility: isLeftButtonShown ? 'visible' : 'hidden' }}>left</button>
+        <CardContainer>
+          {props.outfits.slice(productIndex, productIndex + CAROUSEL_WIDTH).map(product => <OutfitItem product={product} />)}
+        </CardContainer>
+        <button onClick={() => handleClick(true)} style={{visibility: isRightButtonShown ? 'visible' : 'hidden' }}>right</button>
+      </CarouselContainer>
+    </div>
+  );
+}
+
+export default Outfit;
