61c48bc6024c4a60639fced140d94522e6501fda
Ubuntu <ubuntu@ip-172-31-20-18.us-west-1.compute.internal>
2021-08-06 22:55:58 +0000

    webpack prod mode added

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,6 @@ var DIST_DIR = path.join(__dirname, "/client/dist");
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
-  mode: "development",
   output: {
     filename: "bundle.js",
     path: DIST_DIR,
