0c3412b9ffc7bb88cb0d16386d73010860b30859
Preston Carroll <prestondcarroll@gmail.com>
2021-08-07 15:58:36 -0700

    removing style load

--- client/src/components/StylesList.jsx
+++ client/src/components/StylesList.jsx
@@ -34,7 +34,7 @@ const placeHolderStyle = {
   name: 'Forest Green & Black',
   photos: [
     {
-      thumbnail_url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+      thumbnail_url: '',
     },
   ],
 };
