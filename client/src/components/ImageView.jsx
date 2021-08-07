785f348a2209da3226066fcaaaf07c066c1c5eaf
Preston Carroll <prestondcarroll@gmail.com>
2021-08-07 15:46:10 -0700

    no loading image

--- client/src/components/ImageView.jsx
+++ client/src/components/ImageView.jsx
@@ -21,8 +21,8 @@ const placeHolderStyle = {
   name: 'Forest Green & Black',
   photos: [
     {
-      thumbnail_url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
-      url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
+      thumbnail_url: '',
+      url: '',
     },
   ],
 };
